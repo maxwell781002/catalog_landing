cd577d2c1ef235841ea175aa48775ac0c6294165
yunaysy <yunaysy@gmail.com>
2024-10-23 21:51:32 -0400

    Removing French translation files

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -1,7 +1,7 @@
-import ogImageSrc from "@images/social.png";
+import ogImageSrc from "@images/Imagen-1.png";
 
 export const SITE = {
-  title: "En El Garaje",
+  title: "EnElGaraje",
   // tagline: "Top-quality Hardware Tools",
   description: "EnElGaraje es una innovadora aplicación diseñada para permitir a los negocios, crear y gestionar sus propias tiendas online de manera rápida y eficiente.",
   description_short: "EnElGaraje es una innovadora aplicación diseñada para permitir a los negocios, crear y gestionar sus propias tiendas online de manera rápida y eficiente.",
@@ -32,7 +32,7 @@ export const OG = {
   locale: "en_US",
   type: "website",
   url: SITE.url,
-  title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Innovadora aplicación diseñada para permitir a los negocios, crear y gestionar sus propias tiendas online de manera rápida y e ciente",
+  title: `${SITE.title}`,
+  description: "Innovadora aplicación diseñada para permitir a los negocios, crear y gestionar sus propias tiendas online de manera rápida y eficiente",
   image: ogImageSrc,
 };
